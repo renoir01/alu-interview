4aaff392fe80a6cd4116a4a1338cf1eb35bd9223
renoir01 <r.kaze@alustudent.com>
2024-01-30 00:27:18 -0800

    rain project

---
+++ rain/0-rain.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def rain(walls):
+    n = len(walls)
+
+    if n <= 2:
+        return 0
+
+    left_max = [0] * n
+    right_max = [0] * n
+
+    left_max[0] = walls[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i - 1], walls[i])
+
+    right_max[n - 1] = walls[n - 1]
+    for i in range(n - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], walls[i])
+
+    total_water = 0
+    for i in range(1, n - 1):
+        min_height = min(left_max[i], right_max[i])
+        if min_height > walls[i]:
+            total_water += min_height - walls[i]
+
+    return total_water
+
+if __name__ == "__main__":
+    walls = [0, 1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))  # Output: 6
+
+    walls = [2, 0, 0, 4, 0, 0, 1, 0]
+    print(rain(walls))  # Output: 6
